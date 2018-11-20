0860980588a8d88215ba512b505cba94339c99f3
Ed Sanders <ejsanders@gmail.com>
2018-11-20 11:37:39 +0000

    Install jQuery plugin via GitHub to avoid rule prefix
    Ideally we would install via the scoped NPM package,
    but this results in giving all the rules an @wikimedia
    prefix.

--- test/fixtures/jquery/invalid.js
+++ test/fixtures/jquery/invalid.js
@@ -2,25 +2,25 @@
 
 ( function () {
 
-	// eslint-disable-next-line @wikimedia/jquery/no-each-util
+	// eslint-disable-next-line jquery/no-each-util
 	$.each( [], function () {} );
 
-	// eslint-disable-next-line @wikimedia/jquery/no-grep
+	// eslint-disable-next-line jquery/no-grep
 	$.grep( [ 1, 2, 3 ], function () {} );
 
-	// eslint-disable-next-line @wikimedia/jquery/no-in-array
+	// eslint-disable-next-line jquery/no-in-array
 	$.inArray( 1, [ 1 ] );
 
-	// eslint-disable-next-line @wikimedia/jquery/no-is-function
+	// eslint-disable-next-line jquery/no-is-function
 	$.isFunction( function () {} );
 
-	// eslint-disable-next-line @wikimedia/jquery/no-map-util
+	// eslint-disable-next-line jquery/no-map-util
 	$.map( [ 1 ], function () {} );
 
-	// eslint-disable-next-line @wikimedia/jquery/no-proxy
+	// eslint-disable-next-line jquery/no-proxy
 	$.proxy( function () {}, this );
 
-	// eslint-disable-next-line @wikimedia/jquery/no-trim
+	// eslint-disable-next-line jquery/no-trim
 	$.trim( ' foo ' );
 
 }() );
