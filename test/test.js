952c27ae3dd489a03f450fb56b6d1dd522326126
Ed S <ejsanders@gmail.com>
2018-12-14 23:27:54 +0000

    Replace invalid-results.json files with --report-unused-disable-directives (#139)
    Fixes #135

--- test/test.js
+++ test/test.js
@@ -3,12 +3,9 @@
 var fs = require( 'fs' ),
 	assert = require( 'assert' ),
 	assertDiff = require( 'assert-diff' ),
-	eslint = require( 'eslint' ),
 
 	validFixturesFile, validFixtures,
 	invalidFixturesFile, invalidFixtures, testPositivesFailures,
-	expectedFailuresFile, expectedFailures,
-	engine, report, results, prevFilename, prevLine,
 
 	profiles = require( '../package.json' ).files
 		// Trim ".json" from fileName end
@@ -37,49 +34,9 @@ profiles.forEach( function ( profile ) {
 	} );
 	console.log( '\tPositive rules (' + count + ') are covered.' );
 
-	// Test for failures including negative rules
-	count = 0;
-	invalidFixtures = fs.readFileSync( invalidFixturesFile );
-	expectedFailuresFile = __dirname + '/fixtures/' + profile + '/invalid-results.json';
-	expectedFailures = require( expectedFailuresFile );
-	// Verify we got the expected warnings
-	engine = new eslint.CLIEngine( {
-		outputFile: false,
-		maxWarnings: -1,
-		// Disable eslint-disable comments, which we use for coverage
-		allowInlineConfig: false
-	} );
-	report = engine.executeOnFiles( [ invalidFixturesFile ] );
-	results = report.results.map( function ( fileResult ) {
-		return fileResult.messages.map( function ( result ) {
-			var filename, relativeLine;
-
-			filename = fileResult.filePath.slice( __dirname.length + 1 );
-			if ( prevFilename !== filename ) {
-				prevFilename = filename;
-				prevLine = 0;
-			}
-
-			relativeLine = result.line - prevLine;
-			prevLine = result.line;
-
-			return {
-				filename: filename,
-				line: relativeLine,
-				column: result.column,
-				ruleId: result.ruleId
-			};
-		} );
-	} );
-	if ( process.argv[ 2 ] === 'dump' ) {
-		fs.writeFileSync( expectedFailuresFile, JSON.stringify( results, null, '\t' ) );
-		return;
-	}
-	assertDiff.deepEqual( results, expectedFailures, 'Expected warnings' );
-	console.log( '\tVerified ' + results[ 0 ].length + ' expected warnings.' );
-
 	// Verify coverage
 	count = 0;
+	invalidFixtures = fs.readFileSync( invalidFixturesFile );
 	testPositivesFailures = fs.readFileSync( __dirname + '/fixtures/' + profile + '/positiveFailures.json' );
 	Object.keys( config.rules ).forEach( function ( rule ) {
 		var rDisableRule = new RegExp( '(/[/*]) eslint-disable(-next-line)? ([a-z-]+, )??' + rule );
