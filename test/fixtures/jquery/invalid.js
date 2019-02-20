f6c7989e8228cee7eaf3e8ac6265c7d479f6d6cc
Ed Sanders <ejsanders@gmail.com>
2019-02-20 12:27:52 +0000

    Update eslint-plugin-jquery to eslint-plugin-no-jquery 2.0.0
    Rename all jquery/ prefixes to no-jquery/

--- test/fixtures/jquery/invalid.js
+++ test/fixtures/jquery/invalid.js
@@ -1,109 +1,109 @@
 ( function () {
 	function f() {}
 
-	// eslint-disable-next-line jquery/no-and-self
+	// eslint-disable-next-line no-jquery/no-and-self
 	$( [] ).andSelf();
 
-	// eslint-disable-next-line jquery/no-animate
+	// eslint-disable-next-line no-jquery/no-animate
 	$( [] ).animate( { opacity: 0.5 } );
 
-	// eslint-disable-next-line jquery/no-animate-toggle
+	// eslint-disable-next-line no-jquery/no-animate-toggle
 	$( [] ).show( true );
 
-	// eslint-disable-next-line jquery/no-bind
+	// eslint-disable-next-line no-jquery/no-bind
 	$( [] ).bind( 'click', function () {} );
 
-	// eslint-disable-next-line jquery/no-delegate
+	// eslint-disable-next-line no-jquery/no-delegate
 	$( [] ).delegate( 'click', function () {} );
 
-	// eslint-disable-next-line jquery/no-die
+	// eslint-disable-next-line no-jquery/no-die
 	$( [] ).die( 'click', function () {} );
 
-	// eslint-disable-next-line jquery/no-each-util
+	// eslint-disable-next-line no-jquery/no-each-util
 	$.each( [], function () {} );
 
-	// eslint-disable-next-line jquery/no-event-shorthand
+	// eslint-disable-next-line no-jquery/no-event-shorthand
 	$( [] ).click( function () {} );
 
-	// eslint-disable-next-line jquery/no-event-shorthand
+	// eslint-disable-next-line no-jquery/no-event-shorthand
 	$( [] ).keypress( function () {} );
 
-	// eslint-disable-next-line jquery/no-event-shorthand
+	// eslint-disable-next-line no-jquery/no-event-shorthand
 	$( [] ).focus( function () {} );
 
-	// eslint-disable-next-line jquery/no-event-shorthand
+	// eslint-disable-next-line no-jquery/no-event-shorthand
 	$( [] ).scroll( function () {} );
 
-	// eslint-disable-next-line jquery/no-fade
+	// eslint-disable-next-line no-jquery/no-fade
 	$( [] ).fadeIn();
 
-	// eslint-disable-next-line jquery/no-global-eval
+	// eslint-disable-next-line no-jquery/no-global-eval
 	$.globalEval( 'x=3' );
 
-	// eslint-disable-next-line jquery/no-global-selector
+	// eslint-disable-next-line no-jquery/no-global-selector
 	$( '.my-class' );
 
-	// eslint-disable-next-line jquery/no-grep
+	// eslint-disable-next-line no-jquery/no-grep
 	$.grep( [ 1, 2, 3 ], function () {} );
 
-	// eslint-disable-next-line jquery/no-hold-ready
+	// eslint-disable-next-line no-jquery/no-hold-ready
 	$.holdReady( true );
 
-	// eslint-disable-next-line jquery/no-in-array
+	// eslint-disable-next-line no-jquery/no-in-array
 	$.inArray( 1, [ 1 ] );
 
-	// eslint-disable-next-line jquery/no-is-array
+	// eslint-disable-next-line no-jquery/no-is-array
 	$.isArray( [ 1 ] );
 
-	// eslint-disable-next-line jquery/no-is-numeric
+	// eslint-disable-next-line no-jquery/no-is-numeric
 	$.isNumeric( '1' );
 
-	// eslint-disable-next-line jquery/no-is-function
+	// eslint-disable-next-line no-jquery/no-is-function
 	$.isFunction( function () {} );
 
-	// eslint-disable-next-line jquery/no-is-window
+	// eslint-disable-next-line no-jquery/no-is-window
 	$.isWindow( window );
 
-	// eslint-disable-next-line jquery/no-live
+	// eslint-disable-next-line no-jquery/no-live
 	$( [] ).live( 'click', function () {} );
 
-	// eslint-disable-next-line jquery/no-map-util
+	// eslint-disable-next-line no-jquery/no-map-util
 	$.map( [ 1 ], function () {} );
 
-	// eslint-disable-next-line jquery/no-noop
+	// eslint-disable-next-line no-jquery/no-noop
 	f( $.noop );
 
-	// eslint-disable-next-line jquery/no-now
+	// eslint-disable-next-line no-jquery/no-now
 	f( $.now() );
 
-	// eslint-disable-next-line jquery/no-parse-html-literal
+	// eslint-disable-next-line no-jquery/no-parse-html-literal
 	$( '<tag attr="val">' );
 
-	// eslint-disable-next-line jquery/no-parse-json
+	// eslint-disable-next-line no-jquery/no-parse-json
 	$.parseJSON( '{}' );
 
-	// eslint-disable-next-line jquery/no-proxy
+	// eslint-disable-next-line no-jquery/no-proxy
 	$.proxy( function () {}, this );
 
-	// eslint-disable-next-line jquery/no-slide
+	// eslint-disable-next-line no-jquery/no-slide
 	$( [] ).slideDown();
 
-	// eslint-disable-next-line jquery/no-size
+	// eslint-disable-next-line no-jquery/no-size
 	$( [] ).size();
 
-	// eslint-disable-next-line jquery/no-trim
+	// eslint-disable-next-line no-jquery/no-trim
 	$.trim( ' foo ' );
 
-	// eslint-disable-next-line jquery/no-type
+	// eslint-disable-next-line no-jquery/no-type
 	$.type( true );
 
-	// eslint-disable-next-line jquery/no-unbind
+	// eslint-disable-next-line no-jquery/no-unbind
 	$( [] ).unbind( 'click', function () {} );
 
-	// eslint-disable-next-line jquery/no-undelegate
+	// eslint-disable-next-line no-jquery/no-undelegate
 	$( [] ).undelegate( 'click', function () {} );
 
-	// eslint-disable-next-line jquery/no-unique
+	// eslint-disable-next-line no-jquery/no-unique
 	$.unique();
 
 }() );
