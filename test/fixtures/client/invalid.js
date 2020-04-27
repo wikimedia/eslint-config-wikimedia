f25c4ee893e52ab5c0d06aee3fe2d1dfd487c044
Ed Sanders <ejsanders@gmail.com>
2020-04-27 21:22:20 +0100

    Use 'es' plugin in language/ where possible
    Don't bother with syntax errors as these are caught
    by the parser, but having individual rules for builtin
    methods makes them easier to selectively disable.
    
    Also ensure Array.prototype.entries rule doesn't conflict
    with Object.entries.
    
    Part of #224

--- test/fixtures/client/invalid.js
+++ test/fixtures/client/invalid.js
@@ -20,10 +20,84 @@
 	''.codePointAt();
 	// eslint-disable-next-line no-restricted-syntax
 	[].keys();
+	// eslint-disable-next-line no-restricted-syntax
+	[].entries();
+	// eslint-disable-next-line es/no-array-from
+	Array.from();
+	// eslint-disable-next-line es/no-array-of
+	Array.of();
+	// eslint-disable-next-line es/no-math-acosh
+	Math.acosh();
+	// eslint-disable-next-line es/no-math-asinh
+	Math.asinh();
+	// eslint-disable-next-line es/no-math-atanh
+	Math.atanh();
+	// eslint-disable-next-line es/no-math-cbrt
+	Math.cbrt();
+	// eslint-disable-next-line es/no-math-clz32
+	Math.clz32();
+	// eslint-disable-next-line es/no-math-cosh
+	Math.cosh();
+	// eslint-disable-next-line es/no-math-expm1
+	Math.expm1();
+	// eslint-disable-next-line es/no-math-fround
+	Math.fround();
+	// eslint-disable-next-line es/no-math-hypot
+	Math.hypot();
+	// eslint-disable-next-line es/no-math-imul
+	Math.imul();
+	// eslint-disable-next-line es/no-math-log10
+	Math.log10();
+	// eslint-disable-next-line es/no-math-log1p
+	Math.log1p();
+	// eslint-disable-next-line es/no-math-log2
+	Math.log2();
+	// eslint-disable-next-line es/no-math-sign
+	Math.sign();
+	// eslint-disable-next-line es/no-math-sinh
+	Math.sinh();
+	// eslint-disable-next-line es/no-math-tanh
+	Math.tanh();
+	// eslint-disable-next-line es/no-math-trunc
+	Math.trunc();
+	// eslint-disable-next-line es/no-number-epsilon
+	Number.EPSILON.toString();
+	// eslint-disable-next-line es/no-number-isfinite
+	Number.isFinite();
+	// eslint-disable-next-line es/no-number-isinteger
+	Number.isInteger();
+	// eslint-disable-next-line es/no-number-isnan
+	Number.isNaN();
+	// eslint-disable-next-line es/no-number-issafeinteger
+	Number.isSafeInteger();
+	// eslint-disable-next-line es/no-number-maxsafeinteger
+	Number.MAX_SAFE_INTEGER.toString();
+	// eslint-disable-next-line es/no-number-minsafeinteger
+	Number.MIN_SAFE_INTEGER.toString();
+	// eslint-disable-next-line es/no-number-parsefloat
+	Number.parseFloat();
+	// eslint-disable-next-line es/no-number-parseint
+	Number.parseInt();
+	// eslint-disable-next-line es/no-object-assign
+	Object.assign();
+	// eslint-disable-next-line es/no-object-getownpropertysymbols
+	Object.getOwnPropertySymbols();
+	// eslint-disable-next-line es/no-object-is
+	Object.is();
+	// eslint-disable-next-line es/no-string-fromcodepoint
+	String.fromCodePoint();
+	// eslint-disable-next-line es/no-string-raw
+	String.raw();
 
 	// not-es6
 	// eslint-disable-next-line no-restricted-syntax
 	[].includes();
+	// eslint-disable-next-line es/no-object-entries
+	Object.entries();
+	// eslint-disable-next-line es/no-object-getownpropertydescriptors
+	Object.getOwnPropertyDescriptors();
+	// eslint-disable-next-line es/no-object-values
+	Object.values();
 
 	// not-es2016
 	// eslint-disable-next-line no-restricted-properties
