9b1a08d3de9f1dd65b6adde5ed4470df8b4c5081
Ed Sanders <ejsanders@gmail.com>
2020-06-15 16:09:02 +0100

    Introduce jsduck config
    Contains overrides for jsdoc for legacy environments still using jsduck.
    
    Fixes #285

---
+++ test/fixtures/jsduck/valid.js
@@ -0,0 +1,44 @@
+( function () {
+	var APP;
+
+	// In jsduck environments we need to support this use case
+	// to document complex returns, however this triggers
+	// require-returns and require-returns check:
+
+	// Off: jsdoc/require-returns
+	// Off: jsdoc/require-returns-check
+	/**
+	 * @return {Object} foo
+	 * @return {number} foo.bar
+	 */
+	APP.method = function () {
+		return { bar: 7 };
+	};
+
+	// These means we can't catch the following errors:
+
+	// Off: jsdoc/require-returns
+	/**
+	 * @param {number} bar
+	 */
+	APP.method = function ( bar ) {
+		return { bar: bar };
+	};
+
+	// Off: jsdoc/require-returns-check
+	/**
+	 * @return {number}
+	 */
+	APP.method = function () {
+	};
+
+	// Some different aliases are used in jsduck:
+	/**
+	 * @context {jQuery}
+	 * @alternateClassName otherName
+	 * @type {Object}
+	 */
+	APP.method = function () {
+	};
+
+}( this ) );
