3bc4b0b4461d4299cb57528cea622b0b43c10286
Ed Sanders <ejsanders@gmail.com>
2020-09-14 17:36:08 +0100

    qunit: Update eslint-plugin-qunit to 5.0.0
    Added to /recommended:
    * no-early-return (already enabled here)
    * no-negated-ok (already enabled here)
    * no-assert-logical-expresison (disabled)
    * no-conditional-assertions (disabled)
    
    We can consider enabling no-assert-logical-expresison &
    no-conditional-assertions later, but the use cases in
    MW don't seem obviously wrong.

--- test/fixtures/qunit/valid.js
+++ test/fixtures/qunit/valid.js
@@ -2,10 +2,21 @@ QUnit.module( 'Example' );
 
 // Valid: qunit/require-expect
 QUnit.test( '.foo()', function ( assert ) {
-	var x = 'bar';
+	var x = 'bar',
+		y = 'baz';
+
 	// Valid: qunit/no-assert-equal
 	assert.strictEqual( x, 'bar' );
 
 	// Valid: qunit/no-negated-ok
 	assert.notOk( x );
+
+	// Valid: qunit/no-negated-ok
+	if ( x ) {
+		// Off: qunit/no-conditional-assertions
+		assert.ok( x );
+	}
+
+	// Off: qunit/no-assert-logical-expression
+	assert.ok( x && y );
 } );
