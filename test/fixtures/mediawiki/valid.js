27163cc71ab9e7cae37df01ef6933d1dc29840c8
Ed S <ejsanders@gmail.com>
2021-10-29 21:46:45 +0100

    tests: Assert globals in fixtures (#413)
    Fixes: #328

--- test/fixtures/mediawiki/valid.js
+++ test/fixtures/mediawiki/valid.js
@@ -1,20 +1,28 @@
 ( function () {
 	var bar = 'bar1';
 
+	// Global: OO
+	OO.ui.alert( 'hello' );
+
 	// Valid: mediawiki/class-doc
 	// The following classes are used here:
 	// * foo-bar1
 	// * foo-bar2
 	document.body.classList.add( 'foo-' + bar );
 
+	// Global: mw
 	// Valid: mediawiki/msg-doc
 	// The following messages are used here:
 	// * foo-bar1
 	// * foo-bar2
 	mw.msg( 'foo-' + bar );
 
+	// Global: require
 	// Valid: mediawiki/valid-package-file-require
 	require( './invalid.js' );
 	require( './../test.js' );
 	require( 'SomePackage' );
 }() );
+
+// Global: module
+module.exports = {};
--- test/test.js
+++ test/test.js
@@ -2,7 +2,8 @@
 
 const fs = require( 'fs' ),
 	path = require( 'path' ),
-	configs = require( '../package' ).files;
+	configs = require( '../package' ).files,
+	escapeStringRegexp = require( 'escape-string-regexp' );
 
 function getRules( config ) {
 	const rules = Object.assign( {}, config.rules );
@@ -16,6 +17,7 @@ function getRules( config ) {
 
 function getPluginExtends( config ) {
 	let rules = {};
+	let globals = {};
 	const extendsList = Array.isArray( config.extends ) ?
 		config.extends :
 		[ config.extends ];
@@ -36,15 +38,14 @@ function getPluginExtends( config ) {
 			rules,
 			getRules( childConfig )
 		);
+		Object.assign( globals, childConfig.globals );
 		if ( childConfig.extends ) {
-			rules = Object.assign(
-				{},
-				getPluginExtends( childConfig ),
-				rules
-			);
+			const extendsConfig = getPluginExtends( childConfig );
+			rules = Object.assign( {}, extendsConfig.rules, rules );
+			globals = Object.assign( {}, extendsConfig.globals, globals );
 		}
 	} );
-	return rules;
+	return { rules, globals };
 }
 
 configs.forEach( ( configPath ) => {
@@ -74,6 +75,7 @@ configs.forEach( ( configPath ) => {
 		const invalidFixturesFiles = fixturesFiles.filter( ( file ) => file.includes( '/invalid' ) );
 
 		let rules = getRules( config );
+		let globals = config.globals || {};
 
 		if ( configName === 'server' ) {
 			// Load the rules for Node & ES2018 when testing server
@@ -90,11 +92,9 @@ configs.forEach( ( configPath ) => {
 		}
 
 		if ( upstreamConfigsToTest.includes( configName ) ) {
-			rules = Object.assign(
-				{},
-				getPluginExtends( config ),
-				rules
-			);
+			const extendsConfig = getPluginExtends( config );
+			rules = Object.assign( {}, extendsConfig.rules, rules );
+			globals = Object.assign( {}, extendsConfig.globals, globals );
 		}
 
 		function isEnabled( rule ) {
@@ -111,7 +111,7 @@ configs.forEach( ( configPath ) => {
 		Object.keys( rules ).forEach( ( rule ) => {
 			// Disabled rules are covered below
 			if ( isEnabled( rule ) ) {
-				QUnit.test( `Rule ${rule} is covered in invalid fixture`, ( assert ) => {
+				QUnit.test( `Rule '${rule}' is covered in invalid fixture`, ( assert ) => {
 					const rDisableRule = new RegExp( `(/[/*]|<!--) eslint-disable((-next)?-line)? ([a-z-/]+, )*?${rule}($|[^a-z-])` );
 					assert.true( rDisableRule.test( invalidFixtures ), 'eslint-disable comment found' );
 				} );
@@ -125,12 +125,25 @@ configs.forEach( ( configPath ) => {
 		Object.keys( rules ).forEach( ( rule ) => {
 			const rEnableRule = new RegExp( `Off: ${rule}($|[^a-z-])` );
 			if ( !isEnabled( rule ) ) {
-				QUnit.test( `Rule ${rule} is covered as "off" in valid fixture`, ( assert ) => {
-					assert.true( rEnableRule.test( validFixtures ), '"off" comment found' );
+				QUnit.test( `Rule '${rule}' is covered as "off" in valid fixture`, ( assert ) => {
+					assert.true( rEnableRule.test( validFixtures ), '"Off" comment found' );
 				} );
 			} else {
-				QUnit.test( `Rule ${rule} is not covered as "off" in valid fixture`, ( assert ) => {
-					assert.true( !rEnableRule.test( validFixtures ), '"off" comment not present' );
+				QUnit.test( `Rule '${rule}' is not covered as "off" in valid fixture`, ( assert ) => {
+					assert.true( !rEnableRule.test( validFixtures ), '"Off" comment not present' );
+				} );
+			}
+		} );
+
+		Object.keys( globals ).forEach( ( global ) => {
+			const rGlobal = new RegExp( `Global: ${escapeStringRegexp( global )}(\n|$)` );
+			if ( globals[ global ] !== 'off' ) {
+				QUnit.test( `Global '${global}' is documented in valid fixture`, ( assert ) => {
+					assert.true( rGlobal.test( validFixtures ), '"Global" comment found' );
+				} );
+			} else {
+				QUnit.test( `Disabled global '${global}' is documented in invalid fixture`, ( assert ) => {
+					assert.true( rGlobal.test( invalidFixtures ), '"Global" comment found' );
 				} );
 			}
 		} );
