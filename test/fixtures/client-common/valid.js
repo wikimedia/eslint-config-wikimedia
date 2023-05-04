621177a4d6d542463b1921f17b14f4fe087fffe3
Ed Sanders <ejsanders@gmail.com>
2023-05-04 13:32:39 +0100

    client-common: Disable security/detect-possible-timing-attacks
    Fixes #503

--- test/fixtures/client-common/valid.js
+++ test/fixtures/client-common/valid.js
@@ -1,1 +1,10 @@
 /* eslint-env browser */
+
+// eslint-disable-next-line no-implicit-globals, no-unused-vars
+function checkHash( input, cachedValue ) {
+	var hash = JSON.stringify( input );
+	// Off: security/detect-possible-timing-attacks (#503)
+	if ( hash === cachedValue ) {
+		return true;
+	}
+}
