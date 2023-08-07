2f495b55b13925da75443ba4dcdf96dffd78691f
Ed Sanders <ejsanders@gmail.com>
2023-08-07 14:01:23 +0100

    Rename shared configs by grouping in folders
    e.g.
    * client-es5 -> client/es5
    * mediawiki-quint -> mediawiki/qunit
    * vue3-common -> vue3/common
    
    Fixes #525

---
+++ vue2/es6.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* eslint-disable quote-props, quotes */
+module.exports = {
+	"overrides": [ {
+		"files": [ "**/*.vue" ],
+		"extends": [
+			"../vue2/common",
+			"../language/es6"
+		]
+	} ]
+};
