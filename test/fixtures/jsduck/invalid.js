ac14ea471643bbca3465c2861a26c992e13fd399
Ed Sanders <ejsanders@gmail.com>
2023-11-13 14:03:11 +0000

    jsdoc: Update eslint-plugin-jsdoc to 42.0.0
    Fixes #510

--- test/fixtures/jsduck/invalid.js
+++ test/fixtures/jsduck/invalid.js
@@ -1,13 +1,15 @@
 ( function () {
 
-	// eslint-disable-next-line jsdoc/check-tag-names
+	/* eslint-disable jsdoc/check-tag-names */
 	/**
 	 * @alias Bar
 	 */
+	/* eslint-enable jsdoc/check-tag-names */
 
-	// eslint-disable-next-line jsdoc/check-tag-names
+	/* eslint-disable jsdoc/check-tag-names */
 	/**
 	 * @typedef Baz
 	 */
+	/* eslint-enable jsdoc/check-tag-names */
 
 }( this ) );
