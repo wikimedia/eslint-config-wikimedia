8b7b38d4b8782ba3924fe3dc114e44e0a7f4e88a
Ed Sanders <ejsanders@gmail.com>
2024-05-02 16:33:22 +0100

    jquery: Enable no-extend (allowDeep: true)
    Fixes #535

--- test/fixtures/jquery/valid.js
+++ test/fixtures/jquery/valid.js
@@ -6,4 +6,7 @@
 	$( [] ).animate( { scrollLeft: 50 } );
 	$( [] ).animate( { scrollTop: 50, scrollLeft: 50 } );
 
+	// Valid: no-jquery/no-extend ignores deep copy
+	$.extend( true, {}, {} );
+
 }() );
