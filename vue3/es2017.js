117fb8475ff899b0e9cd38b6cbc75075917a71bb
Ed Sanders <ejsanders@gmail.com>
2025-05-13 17:53:30 +0100

    client: Create client/es2017 config

---
+++ vue3/es2017.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* eslint-disable quote-props, quotes */
+module.exports = {
+	"overrides": [ {
+		"files": [ "**/*.vue" ],
+		"extends": [
+			"../vue3/common",
+			"../language/es2017"
+		]
+	} ]
+};
